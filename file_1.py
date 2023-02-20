0e419ec9eef3fbfe458d2d92b60516e0692e39ea
Artem <artemmochalov220@gmail.com>
2023-02-20 15:36:14 +0400

    Changed funtional

--- file_1.py
+++ file_1.py
@@ -1,13 +1,18 @@
 import random
 
 # Comment 0_0
-y = random.randint(1, 100)
-op = random.randint(1, 100)
-print(f"Welcome text! Yor number is: {y} vs opponent's {op}!!!")
-
-if y > op:
-    print("You win!")
-elif y < op:
-    print("Opponent wins!")
-else:
-    print("Draw!")
+while True:
+    y = random.randint(1, 100)
+    op = random.randint(1, 100)
+    print(f"\nWelcome text!\nYor number is: {y} vs Opponent's: {op}!!!")
+    if y > op:
+        print("You win!")
+    elif y < op:
+        print("Opponent wins!")
+    else:
+        print("Draw!")
+    y_n = ''
+    while y_n != 'y' and y_n != 'n':
+        y_n = input("Wanna reroll? Enter y/n:")
+    if y_n == 'n':
+        break
