8b63390d8246cf0218bbfe284e8f7f888eadce98
Artem <artemmochalov220@gmail.com>
2023-02-20 15:11:08 +0400

    Stuct changes

--- file_1.py
+++ file_1.py
@@ -1,4 +1,13 @@
 import random
 
 # Comment 0_0
-print(f"Welcome text! Yor number is: {random.randint(1, 100)} vs opponent's {random.randint(1, 100)}!!!")
+y = random.randint(1, 100)
+op = random.randint(1, 100)
+print(f"Welcome text! Yor number is: {y} vs opponent's {op}!!!")
+
+if y > op:
+    print("You win!")
+elif y < op:
+    print("Opponent wins!")
+else:
+    print("Draw!")
