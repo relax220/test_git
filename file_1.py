abf97b713a543fed6f0a8de335e8447e7da7677a
Artem <artemmochalov220@gmail.com>
2023-02-20 13:59:28 +0400

    random opponent

--- file_1.py
+++ file_1.py
@@ -1,3 +1,3 @@
 import random
 
-print(f"Welcome text! Yor number is: {random.randint(1, 100)}")
+print(f"Welcome text! Yor number is: {random.randint(1, 100)} vs opponent's {random.randint(1, 100)}")
